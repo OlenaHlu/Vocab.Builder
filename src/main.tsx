62148aa4561e6638eef156bb86af1132d0eb0479
OlenaHlu <glushenkova_es@ukr.net>
2025-07-16 23:30:56 +0300

    added depend-s and create some structure for to start

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import "./style.css";
+import App from "./App";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
