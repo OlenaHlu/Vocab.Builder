7cd93db4a07853aa3191f58363f4334d67657c4e
OlenaHlu <glushenkova_es@ukr.net>
2025-10-07 20:36:39 +0300

    working with training page and form

--- src/components/TrainingRoom/TrainingRoom.tsx
+++ src/components/TrainingRoom/TrainingRoom.tsx
@@ -4,24 +4,48 @@ import Icon from "../common/Icon";
 const TrainingRoom = () => {
   return (
     <section>
-      <div>
+      <form>
         <div>
-          <div>
-            <Icon iconName="ua" className={css.icon} />
-            <p>Ukrainian</p>
+          <div className={css.uaPart}>
+            <div>
+              <input
+                type="text"
+                //   value={inputValue}
+                //   onChange={}
+                className={css.input}
+                placeholder="Введіть переклад"
+              />
+            </div>
+            <div className={css.langWithAction}>
+              <div className={css.actionPart}>
+                <p className={css.actionText}>Next</p>
+                <button type="button" className={css.actionBtn}>
+                  <Icon className={css.iconArrow} iconName="arrow" />
+                </button>
+              </div>
+              <div className={css.langPart}>
+                <Icon iconName="ua" className={css.icon} />
+                <p className={css.langText}>Ukrainian</p>
+              </div>
+            </div>
+          </div>
+          <div className={css.ukPart}>
+            <p className={css.ukText}>there will be tasks here later</p>
+            <div className={css.langPart}>
+              <Icon iconName="uk" className={css.icon} />
+              <p className={css.langText}>English</p>
+            </div>
           </div>
         </div>
-        <div>
-          <div>
-            <Icon iconName="uk" className={css.icon} />
-            <p>English</p>
-          </div>
+        <div className={css.buttonsContainer}>
+          <button type="submit" className={css.saveBtn}>
+            Save
+          </button>
+          <button type="button" className={css.cancelBtn}>
+            Cancel
+          </button>
         </div>
-      </div>
-      <div>
-        <button type="submit">Save</button>
-        <button>Cancel</button>
-      </div>
+      </form>
     </section>
   );
 };
